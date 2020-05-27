7c3a8b3d2e25dda7273eceb9d39488bd70a72eea
Julien DAURAT <juju786601@yahoo.fr>
2020-05-28 00:33:38 +0200

    clean et modif code

--- main.py
+++ main.py
@@ -16,25 +16,23 @@ def choix_indice_valeurs_anormales():
     nb_valeurs_anormales = round(nb_valeurs_a_generer * pourcentage_donnees_anormales)
     liste = []
     liste_indices_val_anormale = []
-    print(nb_valeurs_a_generer)
     if(nombre_donnees_anormales_proximite == 1):    # SI LE NOMBRE DE VALEURS ANORMALES CONSECUTIVES EST A 1
         for i in range(nb_valeurs_a_generer):
             liste.append(i)
         liste_indices_val_anormale = sample(liste, int(nb_valeurs_anormales))
     else:                                           # SI LES VALEURS ANORMALES SE SUIVENT
         while(len(liste_indices_val_anormale) < int(nb_valeurs_anormales)):
-            indice_aleatoire = randint(1,nb_valeurs_a_generer + 1)
-
-            if(nombre_donnees_anormales_proximite == 2):
+            indice_aleatoire = randint(1,nb_valeurs_a_generer)
+            if(nombre_donnees_anormales_proximite == 2):            # SI LE NOMBRE DE VALEURS ANORMALES CONSECUTIVES EST A 2
                 if(indice_aleatoire not in liste_indices_val_anormale and indice_aleatoire + 1 not in liste_indices_val_anormale):
                     liste_indices_val_anormale.append(indice_aleatoire)
                     if(len(liste_indices_val_anormale) < nb_valeurs_anormales): liste_indices_val_anormale.append(indice_aleatoire + 1)
-            elif (nombre_donnees_anormales_proximite == 3):
+            elif (nombre_donnees_anormales_proximite == 3):         # SI LE NOMBRE DE VALEURS ANORMALES CONSECUTIVES EST A 3
                 if(indice_aleatoire not in liste_indices_val_anormale and indice_aleatoire + 1 not in liste_indices_val_anormale and indice_aleatoire + 2 not in liste_indices_val_anormale):
                     liste_indices_val_anormale.append(indice_aleatoire)
                     if(len(liste_indices_val_anormale) < nb_valeurs_anormales): liste_indices_val_anormale.append(indice_aleatoire + 1)
                     if(len(liste_indices_val_anormale) < nb_valeurs_anormales): liste_indices_val_anormale.append(indice_aleatoire + 2)
-            elif (nombre_donnees_anormales_proximite == 4):
+            elif (nombre_donnees_anormales_proximite == 4):         # SI LE NOMBRE DE VALEURS ANORMALES CONSECUTIVES EST A 4
                 if(indice_aleatoire not in liste_indices_val_anormale and indice_aleatoire + 1 not in liste_indices_val_anormale and indice_aleatoire + 2 not in liste_indices_val_anormale):
                     liste_indices_val_anormale.append(indice_aleatoire)
                     if(len(liste_indices_val_anormale) < nb_valeurs_anormales): liste_indices_val_anormale.append(indice_aleatoire + 1)
@@ -51,6 +49,10 @@ d = date(year, month, day)
 
 
 data = {}
+
+# **************************#
+#       CONFIGURATION       #
+# **************************#
 
 # PERSONNE 'NORMALE'
 # LUMINOSITE EN %
@@ -79,10 +81,12 @@ mouvement_ecart_depressif = -4000
 
 # NOMBRE DE DONNEES EN DEHORS DE LA NORMALE
 # 0.05 -> 5%
-pourcentage_donnees_anormales = 0.06
+pourcentage_donnees_anormales = 0.1
 
 # PARAMETRE DE PROXIMITE (X DONNEES ANORMALES D'AFFILEE)
 nombre_donnees_anormales_proximite = 3
+
+
 
 nb_valeurs_a_generer = int(input("Merci d'entrer le nombre de donnees a generer : "))
 choix_profil_personne = int(input("Merci de choisir un profil parmi ceux-ci:\n  1 - Normal\n  2 - Anxieux\n  3 - Depressif\n"))
@@ -104,26 +108,24 @@ else:
 calcul_nb_donnees_anormales = round(nb_valeurs_a_generer * pourcentage_donnees_anormales)
 print ("Nombre de valeurs anormales: " + str(calcul_nb_donnees_anormales))
 
-
+# RECUPERATION DE LA LISTE DES INDICES DES DONNEES QUI SERONT ANORMALES
 liste_indices_val_anormales = choix_indice_valeurs_anormales()
-# print(liste_indices_val_anormales)
 
 i = 1
-while i <= nb_valeurs_a_generer:
+while i <= nb_valeurs_a_generer:        # TANT QUE LE NOMBRE DE DONNEES GENEREES EST INFERIEUR A CELUI A GENERER
     date = d.strftime('%d-%m-%Y')
     d = next_date(d)
     data[date] = []
     if(i in liste_indices_val_anormales):   # SI LA VALEUR QUI VA ETRE AJOUTEE EST UNE VALEUR ANORMALE
-        temperature = randint(temperature_moy + temperature_ecart_normal,temperature_moy + (2 * temperature_ecart_normal))
-        luminosite = randint(luminosite_moy + luminosite_ecart_normal, luminosite_moy + (2 * luminosite_ecart_normal))
-        mouvement = randint(mouvement_moy + mouvement_ecart_normal, mouvement_moy + (2 * mouvement_ecart_normal))
-        #print ("Valeur anormale indice: " + str(i) + "\nTemp:" + str(temperature) + "\nLumi:" + str(luminosite) +"\nMouv:" + str(mouvement) + "\n")
+        temperature = randint(temperature_moy - temperature_ecart_normal,temperature_moy + temperature_ecart_normal)
+        luminosite = randint(luminosite_moy - luminosite_ecart_normal, luminosite_moy + luminosite_ecart_normal)
+        mouvement = randint(mouvement_moy - mouvement_ecart_normal, mouvement_moy + mouvement_ecart_normal)
+        print("Valeur anormale: " + str(date) + ":{Temperature: " + str(temperature)  + " - Luminosite: " + str(luminosite) + " - Mouvement: " + str(mouvement) + "}")
 
     else:                                   # SI LA VALEUR N'EST PAS ANORMALE
         temperature = randint(temperature_moy - temperature_ecart_normal,temperature_moy + temperature_ecart_normal)
         luminosite = randint(luminosite_moy - luminosite_ecart_normal, luminosite_moy + luminosite_ecart_normal)
         mouvement = randint(mouvement_moy - mouvement_ecart_normal, mouvement_moy + mouvement_ecart_normal)
-        #print ("Valeur: indice: " + str(i) + "\nTemp:" + str(temperature) + "\nLumi:" + str(luminosite) +"\nMouv:" + str(mouvement) + "\n")
 
     data[date].append({
         'temperature': temperature,
@@ -135,4 +137,3 @@ while i <= nb_valeurs_a_generer:
 # ECRIS L'ENSEMBLE DES DONNEES GENEREES DANS LE FICHIER DATA.JSON
 with open('data.json', 'w') as outfile:
     json.dump(data, outfile)
-
